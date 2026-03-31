4c6b22512d0b0eeb3cb7c09fd775eea57f91a663
prashannakdk9 <prashannakhadka09@gmail.com>
2026-03-31 04:36:28 +0000

    change

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-
 /********************************************
 *	AUTHOR:	Prashanna 
 * COLLABORATORS: 
@@ -24,12 +23,9 @@ CALC hundrdsPlace = num / 100
 CALC remainder = num % 100
 CALC tensPlace = remainder / 10 
 CALC onesPlace = remainder % 10 
-
-
-
-*	
 *********************************************/
 import java.util.Scanner;
+
 public class Main
 {
 
@@ -43,57 +39,61 @@ public class Main
     int num;
     String again = "Y";
 
-    /***** INITIALIZATION SECTION *****/
-    
-    
     /***** INTRO SECTION *****/
-    System.out.println("Hello! This program turns any whole number between 0 and 999 into a digital Quipu.\n");
-
-    while(again.equalsIgnoreCase("Y")){
-
-    /***** INPUT SECTION *****/
-    do{
-      System.out.print("Please enter a number between 0 and 999: ");
-      num = input.nextInt();
-
-      if(num < 0 || num > 999){
-        System.out.println("ERROR: please enter value between 0 - 999\n");
-      }
-
-    }while(num < 0 || num > 999);
-
-    /***** PROCESSING SECTION *****/
-    int hundredsPlace = num / 100;
-    int remainder = num % 100;
-    int tensPlace = remainder / 10;
-    int onesPlace = remainder % 10;
-
-    System.out.println("\nHundreds   = " + hundredsPlace);
-    System.out.println("Tens       = " + tensPlace);
-    System.out.println("Ones       = " + onesPlace);
-
-    /***** OUTPUT SECTION *****/
-    System.out.println("\nYour quipu:");
-    printQuipu(hundredsPlace, tensPlace, onesPlace);
-
-    System.out.print("\nWould you like to make another quipu? [Y/N]: ");
-    again = input.next();
+    System.out.println("Hello! This program turns any whole number between 0 and 999 into a digital Quipu.");
     System.out.println();
 
+    while (again.equalsIgnoreCase("Y"))
+    {
+      /***** INPUT SECTION *****/
+      do
+      {
+        System.out.print("Please enter a number between 0 and 999: ");
+        num = input.nextInt();
+
+        if (num < 0 || num > 999)
+        {
+          System.out.println("ERROR: please enter value between 0 - 999");
+          System.out.println();
+        }
+
+      } while (num < 0 || num > 999);
+
+      /***** PROCESSING SECTION *****/
+      int hundredsPlace = num / 100;
+      int remainder = num % 100;
+      int tensPlace = remainder / 10;
+      int onesPlace = remainder % 10;
+
+      System.out.println("Hundreds   = " + hundredsPlace);
+      System.out.println("Tens       = " + tensPlace);
+      System.out.println("Ones       = " + onesPlace);
+      System.out.println();
+
+      /***** OUTPUT SECTION *****/
+      System.out.println("Your quipu:");
+      printQuipu(hundredsPlace, tensPlace, onesPlace);
+      System.out.println();
+
+      System.out.print("Would you like to make another quipu? [Y/N]: ");
+      again = input.next();
+      System.out.println();
     }
 
     System.out.println("Goodbye!");
-    
   }
 
   /***** STATIC METHODS *****/
-  public static void printKnots(int numKnots){
-    for(int i = 0; i < numKnots ; i++ ){
+  public static void printKnots(int numKnots)
+  {
+    for (int i = 0; i < numKnots; i++)
+    {
       UtilityBelt.printCentered(QUIPU_WIDTH, "*");
     }
   }
 
-  public static void printQuipu( int hundreds, int tens, int ones){
+  public static void printQuipu(int hundreds, int tens, int ones)
+  {
     UtilityBelt.printCentered(QUIPU_WIDTH, "---");
     UtilityBelt.printCentered(QUIPU_WIDTH, "|");
 
@@ -111,14 +111,4 @@ public class Main
 
     UtilityBelt.printCentered(QUIPU_WIDTH, "\u203E");
   }
-
 }
-
-
-
-
-
-
-
-
-
